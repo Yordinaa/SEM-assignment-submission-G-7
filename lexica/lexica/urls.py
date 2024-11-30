b911493d9aae96e0739d436d240fe4e4d7da68c0
Yordinaa <yordinegusu21@gmail.com>
2024-11-30 19:45:21 +0300

    inital commit user management

--- lexica/lexica/urls.py
+++ lexica/lexica/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 
 from django.urls import path, include
-from . import views
+from user import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Add a path for the home page
 
     path('admin/', admin.site.urls),
+    path('', include('user.urls')),  # Include routes from the user app
+
 ]
--- lexica/user/urls.py
+++ lexica/user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ProfileView, RegisterView, LoginView
+from .views import RegisterView, LoginView, ProfileView, home
 
 urlpatterns = [
+    path('', home, name='home'),  # Home view
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),  # Add profile endpoint
-
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
