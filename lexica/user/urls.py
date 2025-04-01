78ff1f49b37053e89e3818bce36218637c16e4e0
Yordanos Negusu <105152673+Yordinaa@users.noreply.github.com>
2025-04-01 21:40:13 +0300

    Delete lexica directory

--- lexica/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView, ProfileView, home
-
-urlpatterns = [
-    path('', home, name='home'),  # Home view
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-]
